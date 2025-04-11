b9ce5832e947a723eabeb46a7cdfc16f0cfc4835
jdeleglise <jdeleglise@hotmail.be>
2025-04-11 14:18:04 +0200

    Update WebAuthnLoginResource.java
    now route can take a roomId param for better redirection

--- src/main/java/org/unamur/elderrings/app/security/WebAuthnLoginResource.java
+++ src/main/java/org/unamur/elderrings/app/security/WebAuthnLoginResource.java
@@ -10,6 +10,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("/webauthn")
@@ -20,11 +21,15 @@ public class WebAuthnLoginResource {
 
   @GET
   @Produces(MediaType.TEXT_HTML)
-  public Response redirectToPasskeyLogin() {
-    Log.info("Redirecting to key only auth");
+  public Response redirectToPasskeyLogin(@QueryParam("roomId") String roomId) {
+    Log.infof("Redirecting to key-only auth%s", 
+        (roomId != null && !roomId.isBlank()) ? " for room " + roomId : "");
 
-    String redirectUri = oidcAppConfig.applications().get(0).client().redirectUris().get(0);
-    String encodedRedirectUri = URLEncoder.encode(redirectUri, StandardCharsets.UTF_8);
+    String baseRedirectUri = oidcAppConfig.applications().get(0).client().redirectUris().get(0);
+    String finalRedirectUri = (roomId != null && !roomId.isBlank())
+            ? baseRedirectUri + "call-room/" + roomId
+            : baseRedirectUri ;
+    String encodedRedirectUri = URLEncoder.encode(finalRedirectUri, StandardCharsets.UTF_8);
     String clientId = oidcAppConfig.clientId() + "-webauthn";
     String authUrl = oidcAppConfig.authServerUrl() + "/protocol/openid-connect/auth";
 
