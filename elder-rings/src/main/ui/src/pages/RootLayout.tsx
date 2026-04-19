9e5267c27ca8790573b8039f2cc336457b8c3030
Lucas Vervloessem <lucas.vervloessem.atc@gmail.com>
2026-04-19 19:27:42 +0200

    fix(accessibility): fix accessibility issues in HomeMenu and sound player
    * Arrow navigation in the home menu
    * sound player for the entire app instead of just the home menu

--- elder-rings/src/main/ui/src/pages/RootLayout.tsx
+++ elder-rings/src/main/ui/src/pages/RootLayout.tsx
@@ -1,4 +1,5 @@
 import Col from "@components/layout/Col";
+import SoundPlayer from "@components/sounds/SoundPlayer";
 import IncomingCallListener from "@components/webrtc/IncomingCallListener";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -8,6 +9,7 @@ export default function RootLayout() {
     <Col className="h-screen w-screen bg-gradient-to-br from-indigo-900 via-sky-800 to-blue-900 overflow-auto">
       <Outlet />
       <IncomingCallListener />
+      <SoundPlayer />
       <ToastContainer />
     </Col>
   );
