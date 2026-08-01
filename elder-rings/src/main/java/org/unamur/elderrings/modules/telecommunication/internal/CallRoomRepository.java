ce755849b51feb577064333508f09f3f689e218b
Lucas Vervloessem <lucas.vervloessem.atc@gmail.com>
2026-08-01 15:30:34 +0200

    fix(contact-request): Fix who can accept a contact request (only the concerned user, staff or its tutor)
    * Before that fix, anybody who guess the request id could accept a contact request which cause a security issue.
    * Contact request was introduced by Jeremy Deleglise.
    * Clean some unused imports in the same time.

--- elder-rings/src/main/java/org/unamur/elderrings/modules/telecommunication/internal/CallRoomRepository.java
+++ elder-rings/src/main/java/org/unamur/elderrings/modules/telecommunication/internal/CallRoomRepository.java
@@ -2,7 +2,6 @@ package org.unamur.elderrings.modules.telecommunication.internal;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 
 import org.unamur.elderrings.modules.telecommunication.api.models.CallRoom;
 import org.unamur.elderrings.modules.telecommunication.api.models.CallRoomId;
@@ -11,7 +10,7 @@ import org.unamur.elderrings.modules.telecommunication.api.models.CallRoomMember
 public interface CallRoomRepository {
 
     Optional<CallRoom> findById(CallRoomId id);
-    
+
     CallRoom create(Set<CallRoomMember> members);
 
     void delete(CallRoomId id);
--- elder-rings/src/main/java/org/unamur/elderrings/modules/user/api/models/Resident.java
+++ elder-rings/src/main/java/org/unamur/elderrings/modules/user/api/models/Resident.java
@@ -2,7 +2,6 @@ package org.unamur.elderrings.modules.user.api.models;
 
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
